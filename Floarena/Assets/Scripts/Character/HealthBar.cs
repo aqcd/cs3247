63de8829ac9b8d88a2878d888528b16ecdd4ced6
Stratostorm <stratostorm@outlook.com>
2022-03-12 13:29:46 +0800

    add health bar

--- Floarena/Assets/Scripts/Character/HealthBar.cs
+++ Floarena/Assets/Scripts/Character/HealthBar.cs
@@ -6,17 +6,21 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider healthBar;
-    public Health playerHealth;
+    public Health objectHealth;
+    public Text healthString;
 
     private void Start()
     {
+        objectHealth = gameObject.transform.parent.parent.gameObject.GetComponent<Health>();
         healthBar = GetComponent<Slider>();
-        healthBar.maxValue = playerHealth.maxHealth;
-        healthBar.value = playerHealth.maxHealth;
+        healthString = gameObject.transform.GetChild(1).transform.GetComponent<Text>();
+        healthBar.maxValue = objectHealth.maxHealth;
+        healthBar.value = objectHealth.maxHealth;
     }
 
     public void SetHealth(int hp)
     {
         healthBar.value = hp;
+        healthString.text = hp.ToString();
     }
 }
