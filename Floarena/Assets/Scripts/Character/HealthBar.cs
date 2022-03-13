a178b31aae461c7c1dfa734452321fdd02142b83
Stratostorm <stratostorm@outlook.com>
2022-03-13 18:47:17 +0800

    Modify Health bar

--- Floarena/Assets/Scripts/Character/HealthBar.cs
+++ Floarena/Assets/Scripts/Character/HealthBar.cs
@@ -8,7 +8,6 @@ public class HealthBar : MonoBehaviour
     public Slider healthBarSlider;
     public Health objectHealth;
     //public Text healthString;
-    public float positionOffset;
     private float barWidth;
     private int division = 100;
 
@@ -17,7 +16,7 @@ public class HealthBar : MonoBehaviour
         //healthString = gameObject.transform.GetChild(1).transform.GetComponent<Text>();
         healthBarSlider.maxValue = objectHealth.maxHealth;
         healthBarSlider.value = objectHealth.maxHealth;
-
+        /*
         RectTransform rt = gameObject.GetComponent<RectTransform>();
         barWidth = rt.rect.width;
 
@@ -28,18 +27,12 @@ public class HealthBar : MonoBehaviour
             int offset_current = i * lineOffset;
             //Draw line using offset_current
         }
+        */
     }
 
     public void SetHealth(int hp)
     {
         healthBarSlider.value = hp;
         //healthString.text = hp.ToString();
-    }
-
-    private void LateUpdate()
-    {
-        if (objectHealth) {
-            transform.position = Camera.main.WorldToScreenPoint(objectHealth.transform.position + Vector3.up * positionOffset);
-        }
     }
 }
