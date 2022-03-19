b1ef8ff7a141f6a88c537a22590d63efd817d101
Stratostorm <stratostorm@outlook.com>
2022-03-19 14:28:04 +0800

    Create VineAttack

---
+++ Floarena/Assets/Scripts/Skill/Projectile/VineAttackProjectile.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class VineAttackProjectile : NetworkBehaviour
+{
+    private float range = SkillConstants.VINE_ATTACK_RANGE;
+    private float projectileSpeed = SkillConstants.VINE_ATTACK_PROJECTILE_SPEED;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeathRoutine());
+    }
+
+
+    [ClientRpc]
+    public void OnSpawn(Vector3 dir) 
+    {
+        rb = transform.GetComponent<Rigidbody>();
+        rb.AddForce(projectileSpeed * dir, ForceMode.VelocityChange);
+    }
+
+    private IEnumerator DeathRoutine() {
+        yield return new WaitForSeconds(range/projectileSpeed);
+        GameObject.Destroy(gameObject);
+    }
+}
