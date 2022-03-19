509712fe07d6fc48293eb5a0b73ebf318b0ad28b
Lauren Lee <e0424895@u.nus.edu>
2022-03-19 13:55:24 +0800

    Remove walls

--- Floarena/Assets/Scripts/Multiplayer/HealthItem.cs
+++ Floarena/Assets/Scripts/Multiplayer/HealthItem.cs
@@ -4,19 +4,5 @@ using UnityEngine;
 using Mirror;
 
 public class HealthItem : NetworkBehaviour {
-    public List<GameObject> liGoSpawn = new List<GameObject>(); // Prefabs for pickup items
-
-    private IEnumerator coroutine;
-
-    private IEnumerator PlacePickupItemWithDelay(float waitTime, Vector3 positionOnGrid) {
-        yield return new WaitForSeconds(waitTime);
-        GameObject goToSpawn = liGoSpawn[Random.Range(0, liGoSpawn.Count)];
-        Instantiate(goToSpawn, positionOnGrid, Quaternion.identity);
-    }
-
-    public void SpawnPickupItem(Vector3 positionOnGrid) {
-        coroutine = PlacePickupItemWithDelay(5.0f, positionOnGrid);
-        StartCoroutine(coroutine);
-    }
 
 }
