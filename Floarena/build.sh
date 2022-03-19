6a4a707b1ddcc2cf736bfc731f44461a2b166447
Raghav-B <raghavbhardwaj1998@gmail.com>
2022-03-20 00:53:22 +0800

    feat: add BuildScript for convenience

---
+++ Floarena/build.sh
@@ -0,0 +1,3 @@
+echo "Starting build..."
+"C:\Program Files\Unity\Hub\Editor\2020.3.26f1\Editor\Unity.exe" -quit -batchmode -projectPath "D:\software_dev\cs3247\Floarena" -executeMethod BuildScript.BuildAll
+echo "Finished build!"
