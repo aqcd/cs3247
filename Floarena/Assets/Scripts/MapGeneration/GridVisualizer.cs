34f2ac2d19018485d336827edc3d8259bc94250d
Lauren Lee <e0424895@u.nus.edu>
2022-03-21 14:20:39 +0800

    Fix map quad

--- Floarena/Assets/Scripts/MapGeneration/GridVisualizer.cs
+++ Floarena/Assets/Scripts/MapGeneration/GridVisualizer.cs
@@ -6,7 +6,7 @@ public class GridVisualizer : MonoBehaviour {
     public GameObject groundPrefab;
 
     public void VisualizeGrid(int width, int length) {
-        Vector3 position = new Vector3(width / 2f, -1f, length / 2f);
+        Vector3 position = new Vector3(width / 2f - 0.5f, -1f, length / 2f - 0.5f);
         Quaternion rotation = Quaternion.Euler(90, 0, 0);
         var board = Instantiate(groundPrefab, position, rotation);
         board.transform.localScale = new Vector3(width, length, 1);
