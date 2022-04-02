60eccc591a9da198e2a0671ddb6b0a97ac513028
aqcd <aquajet173@gmail.com>
2022-04-02 18:38:15 +0800

    integrate pfx

--- Floarena/Assets/Scripts/Berries/UI/BuffIcon.cs
+++ Floarena/Assets/Scripts/Berries/UI/BuffIcon.cs
@@ -7,6 +7,7 @@ public class BuffIcon : MonoBehaviour {
 
     private GameObject player;
     private PlayerManager playerManager;
+    private ParticleSystemManager particleSystemManager;
 
     private float fullDuration;
     private float remainingDuration;
@@ -16,26 +17,43 @@ public class BuffIcon : MonoBehaviour {
     void Start() {
         player = MatchManager.instance.GetPlayer();
         playerManager = player.GetComponent<PlayerManager>();
+        particleSystemManager = player.GetComponent<ParticleSystemManager>();
         image = gameObject.GetComponent<Image>();
         fullDuration = playerManager.GetBuffDuration(attribute);
         remainingDuration = fullDuration;
+
+        
+        switch (attribute) {
+            case Attribute.AD:
+                particleSystemManager.PlayADUP();
+                break;
+            case Attribute.AS:
+                particleSystemManager.PlayASUP();
+                break;
+            case Attribute.MS:
+                particleSystemManager.PlayMSUP();
+                break;
+        }
     }
 
     void Update() {
         remainingDuration = playerManager.GetBuffDuration(attribute);
         image.fillAmount = remainingDuration / fullDuration;
         if (remainingDuration < 0.0f) {
-        switch (attribute) {
-            case Attribute.AD:
-                BuffIconsManager.instance.hasADBuff = false;
-                break;
-            case Attribute.AS:
-                BuffIconsManager.instance.hasASBuff = false;
-                break;
-            case Attribute.MS:
-                BuffIconsManager.instance.hasMSBuff = false;
-                break;
-        }
+            switch (attribute) {
+                case Attribute.AD:
+                    BuffIconsManager.instance.hasADBuff = false;
+                    particleSystemManager.StopADUP();
+                    break;
+                case Attribute.AS:
+                    BuffIconsManager.instance.hasASBuff = false;
+                    particleSystemManager.StopASUP();
+                    break;
+                case Attribute.MS:
+                    BuffIconsManager.instance.hasMSBuff = false;
+                    particleSystemManager.StopMSUP();
+                    break;
+            }
             Destroy(gameObject);
         }
     }
