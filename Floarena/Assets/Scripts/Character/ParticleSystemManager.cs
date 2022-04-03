5776190e8ff41257d9735d584b20c00501f81b85
Raghav-B <raghavbhardwaj1998@gmail.com>
2022-04-03 17:07:52 +0800

    feat: pfx are now networked

--- Floarena/Assets/Scripts/Character/ParticleSystemManager.cs
+++ Floarena/Assets/Scripts/Character/ParticleSystemManager.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
-public class ParticleSystemManager : MonoBehaviour {
+public class ParticleSystemManager : NetworkBehaviour {
     [SerializeField]
     public ParticleSystem ADUP, ASUP, MSUP, HEAL, DMG;
 
@@ -15,39 +16,90 @@ public class ParticleSystemManager : MonoBehaviour {
         DMG.Stop();
     }
 
-    void Update() {
-        
-    }
-
+    [Command(requiresAuthority = false)]
     public void PlayADUP() {
+        RpcPlayADUP();
+    }
+    [ClientRpc]
+    private void RpcPlayADUP() {
         ADUP.Play();
     }
 
+
+
+    [Command(requiresAuthority = false)]
     public void StopADUP() {
+        RpcStopADUP();
+    }
+    [ClientRpc]
+    private void RpcStopADUP() {
         ADUP.Stop();
     }
 
+
+
+    [Command(requiresAuthority = false)]
     public void PlayASUP() {
+        RpcPlayASUP();
+    }
+    [ClientRpc]
+    private void RpcPlayASUP() {
         ASUP.Play();
     }
 
+
+
+    [Command(requiresAuthority = false)]
     public void StopASUP() {
+        RpcStopASUP();
+    }
+    [ClientRpc]
+    private void RpcStopASUP() {
         ASUP.Stop();
     }
 
+
+
+    [Command(requiresAuthority = false)]
     public void PlayMSUP() {
+        RpcPlayMSUP();
+    }
+    [ClientRpc]
+    private void RpcPlayMSUP() {
         MSUP.Play();
     }
 
+
+
+    [Command(requiresAuthority = false)]
     public void StopMSUP() {
+        RpcStopMSUP();
+    }
+    [ClientRpc]
+    private void RpcStopMSUP() {
         MSUP.Stop();
     }
 
+
+
+    [Command(requiresAuthority = false)]
     public void PlayDMG() {
+        RpcPlayDMG();
+    }
+    [ClientRpc]
+    private void RpcPlayDMG() {
         DMG.Play();
     }
 
+
+
+
+    [Command(requiresAuthority = false)]
     public void PlayHeal() {
+        RpcPlayHeal();
+    }
+    [ClientRpc]
+    private void RpcPlayHeal() {
         HEAL.Play();
     }
 }
