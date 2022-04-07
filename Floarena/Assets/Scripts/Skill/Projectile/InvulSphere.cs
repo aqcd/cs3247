4a89b63de73166e9ce3677b1e849f7506b94d066
Stratostorm <stratostorm@outlook.com>
2022-04-07 18:30:17 +0800

    comment out invulsphere script

--- Floarena/Assets/Scripts/Skill/Projectile/InvulSphere.cs
+++ Floarena/Assets/Scripts/Skill/Projectile/InvulSphere.cs
@@ -6,55 +6,55 @@ using Mirror;
 public class InvulSphere : NetworkBehaviour
 {
 
-    private float duration = SkillConstants.INVUL_DURATION;
-    private GameObject spawningPlayer;
-    private Rigidbody rb;
+    // private float duration = SkillConstants.INVUL_DURATION;
+    // private GameObject spawningPlayer;
+    // private Rigidbody rb;
 
-    private float remainingDuration = 0.0f;
-    private IEnumerator deathCoroutine;
-    private bool isSpawningClient;
+    // private float remainingDuration = 0.0f;
+    // private IEnumerator deathCoroutine;
+    // private bool isSpawningClient;
 
-    void Awake()
-    {
-        deathCoroutine = DeathRoutine();
-        StartCoroutine(deathCoroutine);
-    }
+    // void Awake()
+    // {
+    //     deathCoroutine = DeathRoutine();
+    //     StartCoroutine(deathCoroutine);
+    // }
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    // // Start is called before the first frame update
+    // void Start()
+    // {
         
-    }
+    // }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (remainingDuration > 0.0f)
-        {
-            remainingDuration -= Time.deltaTime;
-        }
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     if (remainingDuration > 0.0f)
+    //     {
+    //         remainingDuration -= Time.deltaTime;
+    //     }
 
-        if (remainingDuration <= 0.0f)
-        {
-            GameObject.Destroy(gameObject);
-        }
-    }
+    //     if (remainingDuration <= 0.0f)
+    //     {
+    //         GameObject.Destroy(gameObject);
+    //     }
+    // }
 
-    [ClientRpc]
-    public void OnSpawn(Vector3 dir, int spawnPlayerNum) 
-    {
-        if (MatchManager.instance.GetPlayerNum() == spawnPlayerNum) {
-            spawningPlayer = MatchManager.instance.GetPlayer();
-            isSpawningClient = true;
-        } else {
-            spawningPlayer = MatchManager.instance.GetOpponent();
-            isSpawningClient = false;
-        }
-    }
+    // [ClientRpc]
+    // public void OnSpawn(Vector3 dir, int spawnPlayerNum) 
+    // {
+    //     if (MatchManager.instance.GetPlayerNum() == spawnPlayerNum) {
+    //         spawningPlayer = MatchManager.instance.GetPlayer();
+    //         isSpawningClient = true;
+    //     } else {
+    //         spawningPlayer = MatchManager.instance.GetOpponent();
+    //         isSpawningClient = false;
+    //     }
+    // }
 
-    private IEnumerator DeathRoutine() {
-        yield return new WaitForSeconds(duration);
-        GameObject.Destroy(gameObject);
-    }
+    // private IEnumerator DeathRoutine() {
+    //     yield return new WaitForSeconds(duration);
+    //     GameObject.Destroy(gameObject);
+    // }
 
 }
