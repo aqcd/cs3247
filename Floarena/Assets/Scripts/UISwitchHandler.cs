6ec11b4c9742430cb6634b3d44ef8aca3815f6df
Raghav-B <raghavbhardwaj1998@gmail.com>
2022-04-14 05:51:48 +0800

    fix: waiting for players screen disappearing on switch to online scene

--- Floarena/Assets/Scripts/UISwitchHandler.cs
+++ Floarena/Assets/Scripts/UISwitchHandler.cs
@@ -9,11 +9,19 @@ public class UISwitchHandler : MonoBehaviour {
     public GameObject loadoutScreen;
     public GameObject waitScreen;
 
+    // This gets triggered once more time when the server makes a switch to the "online" scene
+    // which is a duplicate of the "offline" scene
     void Start() {
         if (instance == null) {
             instance = this;
         }
-        DisplayHomeScreen();
+
+        if (GameManager.instance.isNetworkActive) {
+            // If we are in the online scene now, display the waiting for players prompt
+            DisplayWaitScreen();
+        } else {
+            DisplayHomeScreen();
+        }
     }
 
     public void DisplayHomeScreen() {
